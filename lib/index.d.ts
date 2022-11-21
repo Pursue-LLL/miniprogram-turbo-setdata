27a37d28f6b9e9f1626e28419229f1af5a21e9e9
v_vylliu <v_vylliu@tencent.com>
2022-11-21 10:42:58 +0800

    feat: hide the core file temporarily

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="miniprogram-api-typings" />
-
-export declare type Page = WechatMiniprogram.Page.Constructor;
-export declare type Component = WechatMiniprogram.Component.Constructor;
-
--- lib/log.ts
+++
@@ -1,7 +0,0 @@
-export function warn(msg: string, ...args: any[]) {
-  console.warn(`[turbo warn 🚀 ] ${msg}`, ...args);
-}
-
-export function log(msg: string, ...args: any[]) {
-  console.log(`[turbo log 🚀 ] ${msg}`, ...args);
-}
